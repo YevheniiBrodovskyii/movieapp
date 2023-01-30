c65a57afc5f5427ef6e052cd81df2a1b0664f5cb
Vladyslaw Lakhman <zhenyabroddd@gmail.com>
2023-01-30 18:02:04 +0100

    [Dev] Version 0.2 ( Layout )

---
+++ app/services/movie.service.ts
@@ -0,0 +1,23 @@
+import { axiosClassic } from 'api/interceptors'
+import { getMoviesUrl } from 'config/api.config'
+
+import { IMovie } from '@/shared/types/movie.types'
+
+export const MovieService = {
+	async getAll(searchTerm?: string) {
+		return axiosClassic.get<IMovie[]>(getMoviesUrl(``), {
+			params: searchTerm
+				? {
+						searchTerm,
+				  }
+				: {},
+		})
+	},
+
+	async getMostPopularMovies() {
+		const { data: movies } = await axiosClassic.get<IMovie[]>(
+			getMoviesUrl('/most-popular')
+		)
+		return movies
+	},
+}
